e8c595526644226c05bb0247003e5d381ad1440f
Daniel Tull <dt@danieltull.co.uk>
2012-08-12 18:15:16 +0100

    Initial Commit

---
+++ LibC/LibC.h
@@ -0,0 +1,13 @@
+//
+//  LibC.h
+//  LibC
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibC : NSObject
+
+@end
---
+++ LibC/LibC.m
@@ -0,0 +1,13 @@
+//
+//  LibC.m
+//  LibC
+//
+//  Created by Daniel Tull on 12.08.2012.
+//  Copyright (c) 2012 Daniel Tull. All rights reserved.
+//
+
+#import "LibC.h"
+
+@implementation LibC
+
+@end
